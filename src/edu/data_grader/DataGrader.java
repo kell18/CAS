ed3eaef96b1d837faf61d0325f8b2443e697871f
Альебрт Бикеев < >
2016-01-26 00:12:08 +0300

    Made DataGrader for VK comments

---
+++ src/edu/data_grader/DataGrader.java
@@ -0,0 +1,30 @@
+package edu.data_grader;
+
+import edu.json.JSONArray;
+import edu.json.JSONObject;
+
+import java.io.*;
+import java.util.NoSuchElementException;
+
+public class DataGrader {
+    public static void main(String[] args) {
+        VkGrader grader = new VkGrader();
+
+        try {
+            int lentachId = -29534144;
+            JSONArray comments = grader.GatherComments(lentachId, 3).orElseThrow(
+                    () -> new NoSuchElementException("No comments found"));
+            JSONObject output = new JSONObject();
+            output.put("comments", comments);
+            output.put("count", comments.length());
+
+            try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream("res/data/LentachComments.json"), "utf-8"))) {
+                writer.write(output.toString(4));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
